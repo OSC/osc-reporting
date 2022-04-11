88d766ab922a44f9e1eac127e7e78bd62a49c55b
Luke Weiler <lukew25073@gmail.com>
2022-04-11 16:52:58 -0400

    Reload page after 15 minutes

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,11 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails";
 import "./controllers";
+
+window.addEventListener('turbo:load', (event) => {
+    // Reload page after a given amount of time
+    const SECONDS_BEFORE_RELOAD = 15 * 60;
+    setTimeout(() => {
+      Turbo.visit(window.location.toString(), { action: 'replace' });
+    }, 1000 * SECONDS_BEFORE_RELOAD);
+});
