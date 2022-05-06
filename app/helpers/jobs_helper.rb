3bd89d3cc5f0bb7f35d39a179e0e17d82668cc39
Luke Weiler <lukew25073@gmail.com>
2022-05-06 13:39:02 -0400

    align hash values

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -5,14 +5,14 @@ module JobsHelper
     jobs_running = jobs.count { |job| job.status == 'running' }
     jobs_queued = jobs.count { |job| job.status == 'queued' }
     {
-      'gpu_jobs_running':          gpu_jobs_running,
-      'gpu_jobs_queued':         gpu_jobs_queued,
-      'gpu_jobs_running_pct':      (gpu_jobs_running + gpu_jobs_queued != 0) ? (gpu_jobs_running.to_f / (gpu_jobs_running + gpu_jobs_queued) * 100).to_i : 0,
-      'gpu_jobs_queued_pct': (gpu_jobs_running + gpu_jobs_queued != 0) ? (gpu_jobs_queued.to_f / (gpu_jobs_running + gpu_jobs_queued) * 100).to_i : 0,
-      'jobs_running':       jobs_running,
-      'jobs_queued':        jobs_queued,
-      'jobs_running_pct':   (jobs_running + jobs_queued != 0) ? (jobs_running.to_f / (jobs_running + jobs_queued) * 100).to_i : 0,
-      'jobs_queued_pct':    (jobs_running + jobs_queued != 0) ? (jobs_queued.to_f / (jobs_running + jobs_queued) * 100).to_i : 0
+      'gpu_jobs_running':     gpu_jobs_running,
+      'gpu_jobs_queued':      gpu_jobs_queued,
+      'gpu_jobs_running_pct': (gpu_jobs_running + gpu_jobs_queued != 0) ? (gpu_jobs_running.to_f / (gpu_jobs_running + gpu_jobs_queued) * 100).to_i : 0,
+      'gpu_jobs_queued_pct':  (gpu_jobs_running + gpu_jobs_queued != 0) ? (gpu_jobs_queued.to_f / (gpu_jobs_running + gpu_jobs_queued) * 100).to_i : 0,
+      'jobs_running':         jobs_running,
+      'jobs_queued':          jobs_queued,
+      'jobs_running_pct':     (jobs_running + jobs_queued != 0) ? (jobs_running.to_f / (jobs_running + jobs_queued) * 100).to_i : 0,
+      'jobs_queued_pct':      (jobs_running + jobs_queued != 0) ? (jobs_queued.to_f / (jobs_running + jobs_queued) * 100).to_i : 0
     }
   end
 end
