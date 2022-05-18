db47741c807710ff0693c5a223879a11313406a3
Luke Weiler <lukew25073@gmail.com>
2022-05-18 13:38:08 -0400

    Show cluster status on first render and fix bug preventing refreshing twice (#30)
    * added clusters_with_info to active_jobs model to render on page load
    
    * increased duration between cluster info refreshes
    
    * use model methods to update cluster status streams
    
    * moved cluster status id to partial
    
    * removed comments in jobs_controller

--- app/models/active_jobs.rb
+++ app/models/active_jobs.rb
@@ -2,6 +2,23 @@
 
 class ActiveJobs
   class << self
+    def info_from_jobs(jobs)
+      gpu_jobs_running = jobs.count { |job| job.status == 'running' && job.native[:gres].include?("gpu") }
+      gpu_jobs_queued = jobs.count { |job| job.status == 'queued' && job.native[:gres].include?("gpu") }
+      jobs_running = jobs.count { |job| job.status == 'running' }
+      jobs_queued = jobs.count { |job| job.status == 'queued' }
+      {
+        'gpu_jobs_running':     gpu_jobs_running,
+        'gpu_jobs_queued':      gpu_jobs_queued,
+        'gpu_jobs_running_pct': (gpu_jobs_running + gpu_jobs_queued != 0) ? (gpu_jobs_running.to_f / (gpu_jobs_running + gpu_jobs_queued) * 100).to_i : 0,
+        'gpu_jobs_queued_pct':  (gpu_jobs_running + gpu_jobs_queued != 0) ? (gpu_jobs_queued.to_f / (gpu_jobs_running + gpu_jobs_queued) * 100).to_i : 0,
+        'jobs_running':         jobs_running,
+        'jobs_queued':          jobs_queued,
+        'jobs_running_pct':     (jobs_running + jobs_queued != 0) ? (jobs_running.to_f / (jobs_running + jobs_queued) * 100).to_i : 0,
+        'jobs_queued_pct':      (jobs_running + jobs_queued != 0) ? (jobs_queued.to_f / (jobs_running + jobs_queued) * 100).to_i : 0
+      }
+    end
+
     def clusters
       Rails.cache.fetch('clusters', expires_in: 12.hours) do
         OodCore::Clusters.new(
@@ -20,6 +37,15 @@ class ActiveJobs
             'jobs'    => c.job_adapter.info_all
           }
         end
+      end
+    end
+
+    def clusters_with_info
+      all.map do |c|
+        {
+          'cluster_name' => c['cluster'],
+          'info'         => info_from_jobs(c['jobs'])
+        }
       end
     end
   end
