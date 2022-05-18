b2f145ab0b2dc4a71a777d6877de8d6c15d34152
Luke Weiler <lukew25073@gmail.com>
2022-05-18 11:20:18 -0400

    added clusters_with_info to active_jobs model to render on page load

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,7 +2,8 @@ class JobsController < ApplicationController
   def index
     @jobs = Job.all
     @pct_jobs = Job.usage_w_percentage(Job.all_by_usage)
-    @active_jobs = ActiveJobs.all
-    @cluster_ids = ActiveJobs.clusters.map { |c| c.id }
+    #@active_jobs = ActiveJobs.all
+    #@cluster_ids = ActiveJobs.clusters.map { |c| c.id }
+    @clusters_with_info = ActiveJobs.clusters_with_info
   end
 end
