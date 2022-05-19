4d0f8af92940c44ddc0c999b4350fa26417a5c06
Luke Weiler <lukew25073@gmail.com>
2022-05-19 12:06:42 -0400

    Create config singleton (#33)
    * Create config singleton
    
    * Update config/configuration_singleton.rb
    
    Co-authored-by: Jeff Ohrstrom <johrstrom@osc.edu>
    
    * Update cluster_status_job.rb
    
    Co-authored-by: Jeff Ohrstrom <johrstrom@osc.edu>

--- config/boot.rb
+++ config/boot.rb
@@ -2,3 +2,10 @@ ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
 
 require "bundler/setup" # Set up gems listed in the Gemfile.
 require "bootsnap/setup" # Speed up boot time by caching expensive operations.
+
+# load dotenv files before "before_configuration" callback
+require File.expand_path('configuration_singleton', __dir__)
+
+# global instance to access and use
+Configuration = ConfigurationSingleton.new
+Configuration.load_dotenv_files
