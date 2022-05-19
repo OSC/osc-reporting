2e90e70bf3171e8b219b3326d679f79810b84a33
Luke Weiler <lukew25073@gmail.com>
2022-05-19 11:40:40 -0400

    Update cluster_status_job.rb

--- app/jobs/cluster_status_job.rb
+++ app/jobs/cluster_status_job.rb
@@ -12,6 +12,6 @@ class ClusterStatusJob < ApplicationJob
               target:  "cluster_status_#{cluster_id}",
               locals:  { cluster_info: info, cluster_name: cluster_id } }
     Turbo::StreamsChannel.broadcast_replace_to("cluster_status_#{cluster_id}", **opts)
-    self.class.set(wait: Configuration.minutes_between_cluster_status_reload.minutes).perform_later(cluster_id)
+    self.class.set(wait: Configuration.cluster_reload_delay.minutes).perform_later(cluster_id)
   end
 end
