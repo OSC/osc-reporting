b8bb286dc7c44ceac05333e8b22406a5a0f90022
Luke Weiler <lweiler@osc.edu>
2022-05-27 11:53:58 -0400

    added stream to replace app_inspector_histogram

--- app/controllers/app_inspector_controller.rb
+++ app/controllers/app_inspector_controller.rb
@@ -3,4 +3,13 @@ class AppInspectorController < ApplicationController
     @app_names = Job.all_names
     @app_cpus = Job.app_cpus
   end
+
+  def replace_stream
+    app_cpus = Job.app_cpus(10, 'ondemand/sys/dashboard/sys/bc_osc_jupyter')
+    opts = {  partial: 'histogram',
+              locals:  { graph_data: app_cpus['graph_data'], bin_size: app_cpus['bin_size'] } }
+    respond_to do |format|
+      format.turbo_stream { render turbo_stream: turbo_stream.replace('app_inspector_histogram', **opts) }
+    end
+  end
 end
