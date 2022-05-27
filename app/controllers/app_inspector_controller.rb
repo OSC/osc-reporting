e2e63f412d7eee1a7d49bd82fcc1885085645afd
Luke Weiler <lweiler@osc.edu>
2022-05-27 12:57:30 -0400

    add ability to select app with select field

--- app/controllers/app_inspector_controller.rb
+++ app/controllers/app_inspector_controller.rb
@@ -1,11 +1,11 @@
 class AppInspectorController < ApplicationController
   def index
     @app_names = Job.all_names
-    @app_cpus = Job.app_cpus
+    @app_cpus = Job.app_cpus(5, @app_names[0])
   end
 
   def replace_stream
-    app_cpus = Job.app_cpus(params[:bins_count].to_i, 'ondemand/sys/dashboard/sys/bc_osc_jupyter')
+    app_cpus = Job.app_cpus(params[:bins_count].to_i, params[:app_inspector_selector])
     opts = {  partial: 'histogram',
               locals:  { graph_data: app_cpus['graph_data'], bin_size: app_cpus['bin_size'] } }
     respond_to do |format|
