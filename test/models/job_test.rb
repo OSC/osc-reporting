7d7e6bb91f6fdc8e6f25b5690b1ab7afef58216c
Luke Weiler <lweiler@osc.edu>
2022-06-01 13:18:31 -0400

    add newline to test.yml and added test to job_test

--- test/models/job_test.rb
+++ test/models/job_test.rb
@@ -5,6 +5,10 @@ class JobTest < ActiveSupport::TestCase
     assert true
   end
 
+  test "sacct_fields" do
+    assert_equal(Job.sacct_fields, ['Account', 'JobId', 'Elapsed', 'JobName', 'Cluster', 'AllocTRES'])
+  end
+
   test "pct_format" do
     assert_equal(Job.pct_format(0.53), 53)
   end
