432659fa15ac918d8f8b58c215311abd8d3d3c20
Luke Weiler <lweiler@osc.edu>
2022-06-02 15:13:42 -0400

    Add labels to bin count slider

--- app/javascript/controllers/app_inspector_controller.js
+++ app/javascript/controllers/app_inspector_controller.js
@@ -1,9 +1,14 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["form"];
+  static targets = ["form", "binCountLabel", "binCountSlider"];
 
   submitForm(event) {
+    this.formTarget.requestSubmit();
+  }
+
+  updateBinCount(event) {
+    this.binCountLabelTarget.textContent = this.binCountSliderTarget.value;
     this.formTarget.requestSubmit();
   }
 }
