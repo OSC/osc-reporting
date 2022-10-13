4c420fcd0019291b7c105070b6d0e180e42e4541
Luke Weiler <lweiler@osc.edu>
2022-10-13 16:11:12 -0400

    Add ability to hide empty bins

--- app/controllers/app_inspector_controller.rb
+++ app/controllers/app_inspector_controller.rb
@@ -8,7 +8,11 @@ class AppInspectorController < ApplicationController
   def replace_stream
     app_cpus = Job.app_cpus(params[:bins_slider].to_i, params[:app_select], params[:cluster_select])
     opts = {  partial: 'histogram',
-              locals:  { graph_data: app_cpus['graph_data'], bin_size: app_cpus['bin_size'] } }
+              locals:  {
+                graph_data:      app_cpus['graph_data'],
+                bin_size:        app_cpus['bin_size'],
+                hide_empty_bins: params[:hide_empty_bins]
+              } }
     respond_to do |format|
       format.turbo_stream { render turbo_stream: turbo_stream.replace('app_inspector_histogram', **opts) }
     end
