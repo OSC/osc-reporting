c4ca94904f8f677a62cfdae28c4d73977f585d4f
Luke Weiler <lweiler@osc.edu>
2023-01-04 13:26:19 -0500

    Change "1 per" to "max"

--- app/javascript/controllers/app_inspector_controller.js
+++ app/javascript/controllers/app_inspector_controller.js
@@ -9,7 +9,7 @@ export default class extends Controller {
 
   updateBinCount(event) {
     // If bin slider is set to 21, show "1 per" as number of bins, otherwise show the set value
-    this.binCountLabelTarget.textContent = this.binCountSliderTarget.value == 21 ? "1 per" : this.binCountSliderTarget.value;
+    this.binCountLabelTarget.textContent = this.binCountSliderTarget.value == 21 ? "max" : this.binCountSliderTarget.value;
     this.formTarget.requestSubmit();
   }
 
